51e5dcab5c9edcb0ec88d3794d385c05a13f69dc
Noya Lasheen <118875123+NoyaLasheen@users.noreply.github.com>
2023-02-16 08:15:13 +0530

    Add files via upload

---
+++ SumWithoutInput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name:\tNoya Lasheen\nReg No:\t12218742\nSection:\tKOC CA\n");
+    int a = 10, b = 20, c;
+    c = a+b;
+    printf("Sum of %d and %d is %d\n",a,b,c);
+
+
+    return 0;
+}
---
+++ productWithoutInput.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    int a = 2, b = 3, c = 4;
+    int d;
+    d=a*b*c;
+    printf("Product of %d, %d and %d is %d",a,b,c,d);
+}
